43c3ef5168f228829ce0f9ebcaea35f24c04de0f
Hieleke <hielke.kanger@student.graafschapcollege.nl>
2024-03-12 09:48:29 +0100

    globals change

--- config/globals.php
+++ config/globals.php
@@ -1,5 +1,5 @@
 <?php
 const servername = "localhost";
-const username = "root";
-const password = "";
-const dbname = "bootverhuur_winkel";
+const username = "net24hkanger_net24hkanger";
+const password = "yiAW1+2WN)Er";
+const dbname = "net24hkanger_bootverhuur";
